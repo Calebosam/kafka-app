608771c20c3c51992816950aa92b747b6675ddbe
calebosam <caleb.osam@amalitech.com>
2023-07-07 21:04:19 +0000

    feat: add init applications setup

---
+++ app1/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const dbConfig = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log(`App1 Db connection successful`);
+    })
+    .catch((err) => console.log(err));
+};
+
+const dbsRunning = async () => {
+  await dbConfig();
+};
+
+setTimeout(dbsRunning, 10000);
+
+app.get("/", (req, res, next) => {
+  res.json({ status: "success", message: "App1 is working" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
